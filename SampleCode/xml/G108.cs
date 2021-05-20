befa85d1fc25a36ee8de1cf369235241ad698a56
rol2728 <56250823+rol2728@users.noreply.github.com>
2021-05-20 12:25:49 +0900

    Revert "/.."
    This reverts commit 42193e16c0ea73d04aab4869c45dea52f0a586dd.

--- SampleCode/xml/G108.cs
+++ SampleCode/xml/G108.cs
@@ -72,15 +72,6 @@ namespace NTS_Reader_CS.xml
 
             string emp_no = ""; ;
 
-            foreach (var 인별 in entity.인별)
-            {
-                Dictionary<string, object> resultMap = ReadSql($"select * from QE023DT WHERE ycal_resi = fn_za010ms_03('{인별.resid}') and ycal_year = '{calYear}' and YCAL_RERA='0' ");
-                if (resultMap.Count > 0)
-                {
-                    emp_no = resultMap["EMP_NO"].ToString(); //사번
-                }
-            }
-
 
             //전체합계컬럼 초기화
             executeSql($@"                                      
@@ -111,6 +102,16 @@ namespace NTS_Reader_CS.xml
                                    ,YCAL_BOOK_AMT = 0       
                                 WHERE EMP_NO = '{emp_no}' and YCAL_YEAR={calYear}
                         ");
+
+
+            foreach (var 인별 in entity.인별)
+            {
+                Dictionary<string, object> resultMap = ReadSql($"select * from QE023DT WHERE ycal_resi = fn_za010ms_03('{인별.resid}') and ycal_year = '{calYear}' and YCAL_RERA='0' ");
+                if (resultMap.Count > 0)
+                {
+                    emp_no = resultMap["EMP_NO"].ToString(); //사번
+                }
+            }
 
 
             foreach (var 인별 in entity.인별)
