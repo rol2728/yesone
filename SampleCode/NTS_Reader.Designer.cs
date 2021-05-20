cd18e085ffc0635b7b85f61229b681573f0af2cd
rol2728 <56250823+rol2728@users.noreply.github.com>
2021-05-20 13:33:03 +0900

    폼 디자인 변경

--- SampleCode/NTS_Reader.Designer.cs
+++ SampleCode/NTS_Reader.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.grpPdf = new System.Windows.Forms.GroupBox();
-            this.btnUtf8 = new System.Windows.Forms.Button();
+            this.tb_empNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnPdf = new System.Windows.Forms.Button();
             this.txtPdf = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.grpUtf8 = new System.Windows.Forms.GroupBox();
             this.txtUtf8 = new System.Windows.Forms.RichTextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tb_empNo = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnUtf8 = new System.Windows.Forms.Button();
             this.grpPdf.SuspendLayout();
-            this.grpUtf8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // grpPdf
@@ -49,25 +49,34 @@
             this.grpPdf.Controls.Add(this.btnPdf);
             this.grpPdf.Controls.Add(this.txtPdf);
             this.grpPdf.Controls.Add(this.label1);
-            this.grpPdf.Location = new System.Drawing.Point(12, 12);
+            this.grpPdf.Location = new System.Drawing.Point(271, 6);
             this.grpPdf.Name = "grpPdf";
-            this.grpPdf.Size = new System.Drawing.Size(818, 137);
+            this.grpPdf.Size = new System.Drawing.Size(559, 197);
             this.grpPdf.TabIndex = 0;
             this.grpPdf.TabStop = false;
             // 
-            // btnUtf8
+            // tb_empNo
             // 
-            this.btnUtf8.Location = new System.Drawing.Point(612, 14);
-            this.btnUtf8.Name = "btnUtf8";
-            this.btnUtf8.Size = new System.Drawing.Size(138, 37);
-            this.btnUtf8.TabIndex = 1;
-            this.btnUtf8.Text = "XML 읽기 (UTF-8)";
-            this.btnUtf8.UseVisualStyleBackColor = true;
-            this.btnUtf8.Click += new System.EventHandler(this.btnUtf8_Click);
+            this.tb_empNo.BackColor = System.Drawing.Color.White;
+            this.tb_empNo.Location = new System.Drawing.Point(100, 56);
+            this.tb_empNo.Name = "tb_empNo";
+            this.tb_empNo.ReadOnly = true;
+            this.tb_empNo.Size = new System.Drawing.Size(68, 21);
+            this.tb_empNo.TabIndex = 4;
+            this.tb_empNo.Text = "10110007";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "사번";
             // 
             // btnPdf
             // 
-            this.btnPdf.Location = new System.Drawing.Point(479, 20);
+            this.btnPdf.Location = new System.Drawing.Point(305, 20);
             this.btnPdf.Name = "btnPdf";
             this.btnPdf.Size = new System.Drawing.Size(85, 23);
             this.btnPdf.TabIndex = 2;
@@ -79,73 +88,68 @@
             // 
             this.txtPdf.Location = new System.Drawing.Point(100, 21);
             this.txtPdf.Name = "txtPdf";
-            this.txtPdf.Size = new System.Drawing.Size(373, 21);
+            this.txtPdf.Size = new System.Drawing.Size(199, 21);
             this.txtPdf.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Location = new System.Drawing.Point(26, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "PDF 파일 : ";
             // 
-            // grpUtf8
-            // 
-            this.grpUtf8.Controls.Add(this.txtUtf8);
-            this.grpUtf8.Location = new System.Drawing.Point(12, 155);
-            this.grpUtf8.Name = "grpUtf8";
-            this.grpUtf8.Size = new System.Drawing.Size(818, 349);
-            this.grpUtf8.TabIndex = 1;
-            this.grpUtf8.TabStop = false;
-            this.grpUtf8.Text = "XML (UTF-8)";
-            // 
             // txtUtf8
             // 
             this.txtUtf8.BackColor = System.Drawing.SystemColors.Window;
             this.txtUtf8.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtUtf8.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtUtf8.Location = new System.Drawing.Point(6, 118);
+            this.txtUtf8.Location = new System.Drawing.Point(12, 227);
             this.txtUtf8.Name = "txtUtf8";
             this.txtUtf8.ReadOnly = true;
-            this.txtUtf8.Size = new System.Drawing.Size(812, 299);
+            this.txtUtf8.Size = new System.Drawing.Size(818, 277);
             this.txtUtf8.TabIndex = 0;
             this.txtUtf8.Text = "";
+            this.txtUtf8.Visible = false;
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "사번";
+            this.pictureBox1.Image = global::NTS_Reader_CS.Properties.Resources.yeonmal;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 191);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
-            // tb_empNo
+            // btnUtf8
             // 
-            this.tb_empNo.Location = new System.Drawing.Point(100, 86);
-            this.tb_empNo.Name = "tb_empNo";
-            this.tb_empNo.ReadOnly = true;
-            this.tb_empNo.Size = new System.Drawing.Size(100, 21);
-            this.tb_empNo.TabIndex = 4;
-            this.tb_empNo.Text = "10110007";
+            this.btnUtf8.Image = global::NTS_Reader_CS.Properties.Resources.upload1;
+            this.btnUtf8.Location = new System.Drawing.Point(435, 14);
+            this.btnUtf8.Name = "btnUtf8";
+            this.btnUtf8.Size = new System.Drawing.Size(101, 107);
+            this.btnUtf8.TabIndex = 1;
+            this.btnUtf8.UseVisualStyleBackColor = true;
+            this.btnUtf8.Click += new System.EventHandler(this.btnUtf8_Click);
             // 
             // NTS_Reader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(842, 516);
-            this.Controls.Add(this.grpUtf8);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(842, 216);
+            this.Controls.Add(this.txtUtf8);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.grpPdf);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "NTS_Reader";
             this.ShowIcon = false;
-            this.Text = "NTS_Reader";
+            this.Text = "편리한 연말정산 Muju";
             this.grpPdf.ResumeLayout(false);
             this.grpPdf.PerformLayout();
-            this.grpUtf8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -153,14 +157,14 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grpPdf;
-        private System.Windows.Forms.GroupBox grpUtf8;
         private System.Windows.Forms.Button btnPdf;
         private System.Windows.Forms.TextBox txtPdf;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnUtf8;
-        private System.Windows.Forms.RichTextBox txtUtf8;
         private System.Windows.Forms.TextBox tb_empNo;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox txtUtf8;
     }
 }
 
--- SampleCode/Properties/Resources.Designer.cs
+++ SampleCode/Properties/Resources.Designer.cs
@@ -59,5 +59,35 @@ namespace NTS_Reader_CS.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap upload {
+            get {
+                object obj = ResourceManager.GetObject("upload", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap upload1 {
+            get {
+                object obj = ResourceManager.GetObject("upload1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap yeonmal {
+            get {
+                object obj = ResourceManager.GetObject("yeonmal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
